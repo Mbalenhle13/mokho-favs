e74c67cfee8765d4632329fe21a54f1debe52565
mbalenhle.khokhoba <mbalenhle.khokhoba@entelect.co.za>
2022-10-07 08:44:05 +0200

    added initial set up

---
+++ mokho-faves/src/components/TopBar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-orange-100 w-screen text-center">
+        <p class="text-xs">Top Bar Nav</p>
+    </div>
+</template>
--- mokho-faves/src/main.js
+++ mokho-faves/src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import "/node_modules/primeflex/primeflex.css";
+
 import App from './App.vue'
 import router from './router'
 
